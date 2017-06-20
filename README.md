36bd4b844bb7a896b1d9f79ee2f8df72639a0e40
Jill-Jênn Vie <vie@jill-jenn.net>
2017-06-20 16:09:22 +0900

    Make code PEP8 compliant

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # markov.py
 
-- Compositeur de La Faute à l'algo
+- Compositeur de [La Faute à l'algo](http://fautealgo.fr)
 - <insérer une application accrocheuse ici>
 
-## Usage
+## Install
 
     python3 -m venv venv
     . venv/bin/activate
-    pip install -r requirements.txt
-    python markov.py
+    pip install -r requirements.txt  # Install lea (discrete probability distributions)
+
+## Usage
+
+    python markov.py <text|music|word>
+
+- L'output de `music` est à jouer dans [trinket.io/music](https://trinket.io/music).
+- L'output de `word` se base sur [une liste de 336531 mots du dictionnaire](http://www.pallier.org/extra/liste.de.mots.francais.frgut.txt).
 
 ## Exemple
 
-    je
-    mange
-    des
-    camions
+    je mange des camions
 
 Cf. La Faute à l'algo épisode 1 sur YouTube : [génération procédurale et chaîne de Markov](https://www.youtube.com/watch?v=ngnCE2fCvl4).
--- markov.py
+++ markov.py
@@ -1,23 +1,61 @@
 from lea import Lea
-from collections import Counter
+from collections import Counter, defaultdict
+import argparse
+import os
 
 
-sentences = ['je mange des cerises', 'je mange des bananes', 'je conduis des camions']
-next_one = {}
-for sentence in sentences:
-    words = sentence.split()
-    n = len(words)
-    for i in range(n - 1):
-        if words[i] not in next_one:
-            next_one[words[i]] = Counter()
-        next_one[words[i]][words[i + 1]] += 1
-states = {}
-for word in next_one:
-    states[word] = Lea.fromValFreqsDict(next_one[word])
-# print(states)
+WORD_LIST_URL = 'http://www.pallier.org/extra/liste.de.mots.francais.frgut.txt'
 
-word = 'je'
-for _ in range(3):
+
+def markov(corpus, start, length):
+    # Counting occurrences
+    next_one = defaultdict(Counter)
+    for sentence in corpus:
+        words = sentence.split()
+        nb_words = len(words)
+        for i in range(nb_words - 1):
+            next_one[words[i]][words[i + 1]] += 1
+
+    # Initializing states
+    states = {}
+    for word in next_one:
+        states[word] = Lea.fromValFreqsDict(next_one[word])
+
+    # Outputting visited states
+    word = start
+    for _ in range(length):
+        print(word, end=' ')
+        word = states[word].random()
     print(word)
-    word = states[word].random()
-print(word)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Displays a demo.')
+    parser.add_argument('demo_type', type=str, nargs='?', default='text',
+                        help='a type of demo: "text", "music" or "word"')
+    args = parser.parse_args()
+
+    if args.demo_type == 'text':
+        # Generating sentences word by word
+        corpus = ['je mange des cerises',
+                  'je mange des bananes',
+                  'je conduis des camions']
+        start = 'je'
+        length = 3
+    elif args.demo_type == 'music':
+        # Generating music note by note
+        corpus = ['e d# e d# e b d c a',  # Lettre à Élise de Beethoven
+                  'C E g c e g c e C E g c e g c e C D a d f a d f']  # Bach
+        start = 'e'
+        length = 20
+    elif args.demo_type == 'word':
+        # Generating words letter by letter
+        if not os.path.isfile('liste.de.mots.francais.frgut.txt'):
+            print('Downloading {}…'.format(WORD_LIST_URL))
+            os.system('wget {}'.format(WORD_LIST_URL))
+        corpus = [' '.join(list(word)) for word in
+                  open(os.path.basename(WORD_LIST_URL)).read().splitlines()]
+        start = 'a'
+        length = 12
+
+    markov(corpus, start, length)
