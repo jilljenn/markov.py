94b28dd0f6661175590a8065cf11fc494ffc966d
Jill-Jênn Vie <vie@jill-jenn.net>
2022-04-21 00:32:35 +0200

    Simplify and improve with beginning-of-sequence token

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: demo/words.txt
 
 demo/words.txt: demo/liste.de.mots.francais.frgut.txt
-	iconv -t ASCII//TRANSLIT $< | sort | uniq | sed 's/./& /g' > $@
+	iconv -t ASCII//TRANSLIT $< | sort | uniq | tr -d '\015' | sed 's/./& /g' > $@
 
 demo/liste.de.mots.francais.frgut.txt:
 	wget http://www.pallier.org/extra/liste.de.mots.francais.frgut.txt -O demo/liste.de.mots.francais.frgut.txt
--- markov.py
+++ markov.py
@@ -4,41 +4,43 @@ import random
 from lea import Lea
 
 
+BOS_TOKEN = -2
+EOS_TOKEN = -1
+
+
 def markov(corpus, n_seq=1, start=None, length=42):
     # Counting occurrences
     next_one = defaultdict(Counter)
+    next_one[EOS_TOKEN][EOS_TOKEN] = 1  # Last state is absorbing
     for sentence in corpus:
         words = sentence.split()
         nb_words = len(words)
+        next_one[BOS_TOKEN][words[0]] += 1
         for i in range(nb_words - 1):
             next_one[words[i]][words[i + 1]] += 1
         if nb_words:
             final_word = words[nb_words - 1]
-            next_one[final_word][-1] += 1
-            next_one[-1][-1] += 1  # Last state is absorbing
+            next_one[final_word][EOS_TOKEN] += 1
 
     # Initializing states
     states = {}
-    for word in next_one:
-        states[word] = Lea.fromValFreqsDict(next_one[word])
+    for state in next_one:
+        states[state] = Lea.fromValFreqsDict(next_one[state])
 
     # Outputting visited states
     for _ in range(n_seq):
-        word = start if start is not None else random.choice(list(states.keys()))
-        for _ in range(length):
-            if word == -1:
-                break
-            print(word, end=' ')
-            word = states[word].random()
-        if word != -1:
-            print(word, end='')
-        print()
+        state = start if start is not None else BOS_TOKEN
+        seq = [state]
+        while len(seq) < length and state != EOS_TOKEN:
+            state = states[state].random()
+            seq.append(state)
+        print(' '.join(filter(lambda x: x not in {BOS_TOKEN, EOS_TOKEN}, seq)))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generates tokens.')
     parser.add_argument('filename', type=str, nargs='?', default='text',
-                        help='For a demo, try files in demo/ or "demo:word"')
+                        help='Try files in demo/ e.g. "demo/text.txt"')
     parser.add_argument('--n', type=int, nargs='?', default=1,
                         help='How many sequences should be printed')
     parser.add_argument('--l', type=int, nargs='?', default=42,
@@ -50,10 +52,7 @@ if __name__ == '__main__':
 
     start = None
     length = args.l
-    if args.filename == 'demo/text.txt':  # Generating sentences word by word
-        start = 'je'
-        length = 3
-    elif args.filename == 'demo/music.txt':  # Generating music note by note
+    if args.filename == 'demo/music.txt':  # Generating music note by note
         # This corpus contains "Für Elise" from Beethoven, and some Bach
         start = 'e'
         length = 20
